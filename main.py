ad776d7877b96bbacfea552cc3e596d4db2cea7d
Guilherme Parreiras <parreirasgl@hotmail.com>
2022-01-15 22:42:58 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+# Create a program that asks the user to type in an interval,
+# and that finds the prime numbers in that interval.
+# Criar um programa que peça para o usuário digitar um intervalo,
+# e que encontre os números primos neste intervalo.
