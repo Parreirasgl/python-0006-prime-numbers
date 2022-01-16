115a3aeaa9fddaa4b7c32c0b2f65b6882adeb06a
Guilherme Parreiras <parreirasgl@hotmail.com>
2022-01-15 23:26:50 -0300

    Initial text messages

--- main.py
+++ main.py
@@ -2,3 +2,13 @@
 # and that finds the prime numbers in that interval.
 # Criar um programa que peça para o usuário digitar um intervalo,
 # e que encontre os números primos neste intervalo.
+
+# Display initial text messages:
+# Exibir mensagens de texto inicias:
+print("Write the range in which to search for prime numbers.")
+print("")
+initial_number = input("Enter the initial number in the interval:")
+last_number = input("Enter the last number:")
+print("")
+print("The prime numbers in this range are:")
+
